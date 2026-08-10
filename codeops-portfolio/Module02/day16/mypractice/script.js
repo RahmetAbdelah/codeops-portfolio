6ff38ef2e14d63f1aadba7f332aa8563bb899875
RahmetAbdelah <rahmetmera5@gmail.com>
2026-08-10 21:46:02 +0300

    day 17

---
+++ codeops-portfolio/Module02/day16/mypractice/script.js
@@ -0,0 +1,63 @@
+// //console.log("hello world")
+
+
+// // variable declararion
+// // var:- can be reinitialized
+// // let can be reassigned but not reiniti      allized
+// // const  when we have stagnant value
+
+// //
+// // y=x++
+ 
+// // console.log( y)
+// // console.log(x)
+
+
+// // let name ="rahmet"
+// // console.log(`hello ${name}`)
+
+
+// // 
+
+// let x=10
+// let y=11
+
+// if(x<y){
+//     console.log("wooo");
+// }
+// else if (x<y){
+//     console.log("nooo");
+// }
+
+
+
+// let payment="cbe"
+
+// switch(payment){
+//     case "cbe":
+//         console.log("it is cbe")
+//         break;
+//     case "telebir":
+//         console.log("it is telebir")
+//         break;
+//     default:
+//         console.log("none dear")
+// }
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let menu=["firfir","egg","pancakes"]
+
+// for(item of menu){
+    
+//     console.log(item);
+// }
---
+++ codeops-portfolio/Module02/day17/my_pracrice/function.js
@@ -0,0 +1,56 @@
+// function greet( name){
+//     console.log("good morning {name}");
+// }
+
+// const calc=(n)=>{return 1*n}
+// num=10;
+
+// const greet =(num) =>num*10
+
+// greet();
+
+// function hello(){
+//     let number=0
+//     return function inner(){
+//         console.log(`hello ${number}++`)
+
+//     }
+//     return inner
+// }
+
+// hello();
+// console.log(hello());
+
+
+// const bank=hello();
+// bank.next();
+// function sum(a,b){
+//     return a+b
+// }
+// function sub(a,b){
+//     return a-b
+// }
+// function div(a,b){
+//     return a/b
+// }
+// function multi(a,b){
+//     return a*b
+// }
+
+// function adder(num1,num2,fun){
+//      return fun(num1,num2)
+       
+// }
+
+// console.log(adder(3,2,sum));
+// console.log(adder(4,5,sub));
+// console.log(adder(4,5,div));
+// console.log(adder(6,7,multi));
+
+function makeRecietMaker(){
+    let orderNo=0;
+    const memberoff=disscountby(0.1)
+    return function(
+
+    )
+   }
