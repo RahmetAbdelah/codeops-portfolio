9c902798749555c0aaa4d1445720290363e7340f
RahmetAbdelah <rahmetmera5@gmail.com>
2026-08-12 19:39:15 +0300

    creating an array then adding rendering funtion

---
+++ codeops-portfolio/Module02/day19/assignment/app.js
@@ -0,0 +1,20 @@
+const groceries = [];
+
+const groceries = [
+    { id: 1, name: "Teff", bought: false },
+    { id: 2, name: "Milk", bought: false }
+];
+
+const list = document.querySelector("#list");
+
+function render() {
+    list.innerHTML = "";
+
+    groceries.forEach(item => {
+        const li = document.createElement("li");
+
+        li.textContent = item.name;
+
+        list.appendChild(li);
+    });
+}
